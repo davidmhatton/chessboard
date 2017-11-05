dfd39b3f37d1cb0aaf2040420b73d6902246db95
David Hatton <dhatton@turnitin.com>
2017-11-05 15:45:39 +0000

    master piece

--- src/main/java/models/pieces/Piece.java
+++ src/main/java/models/pieces/Piece.java
@@ -6,4 +6,13 @@ package models.pieces;
  * @author David Hatton
  */
 public class Piece {
+
+	private String name;
+	private boolean taken;
+	private boolean pinned;
+	// private Square location;
+
+	public Piece() {
+	}
+
 }
